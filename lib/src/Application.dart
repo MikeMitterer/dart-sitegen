c9a5f116e4aa5b6216e8ea9941f9820f019c9b56
Mike Mitterer <office@mikemitterer.at>
2015-03-29 16:04:26 +0200

    Server funkt

--- lib/src/Application.dart
+++ lib/src/Application.dart
@@ -4,6 +4,7 @@ class Application {
     final Logger _logger = new Logger("sitegen.Application");
 
     final Options options;
+    Timer timerForPageRefresh = null;
 
     Application() : options = new Options();
 
@@ -20,39 +21,43 @@ class Application {
                 return;
             }
 
-            if(argResults.wasParsed(Options._ARG_SETTINGS)) {
+            if (argResults.wasParsed(Options._ARG_SETTINGS)) {
                 config.printSettings();
                 return;
             }
 
-            if(argResults.wasParsed(Options._ARG_TEST)) {
-                _refreshPage(config);
+            if (argResults.wasParsed(Options._ARG_TEST)) {
+                _testPWD(config.outputfolder);
+                _testPWD2();
+
                 return;
             }
 
             bool foundOptionToWorkWith = false;
 
-            if(argResults.wasParsed(Options._ARG_GENERATE)) {
+            if (argResults.wasParsed(Options._ARG_GENERATE)) {
                 foundOptionToWorkWith = true;
                 new Generator().generate(config);
             }
 
-            if(argResults.wasParsed(Options._ARG_WATCH)) {
+            if (argResults.wasParsed(Options._ARG_WATCH)) {
                 foundOptionToWorkWith = true;
-                new Generator().generate(config);
-                watch(config.contentfolder,config);
-                watch(config.templatefolder,config);
-                watchToRefresh(config.outputfolder,config);
-                watchScss(config.outputfolder,config);
+                if (_isFolderAvailable(config.contentfolder) && _isFolderAvailable(config.templatefolder)) {
+                    watch(config.contentfolder, config);
+                    watch(config.templatefolder, config);
+                    new Generator().generate(config);
+                }
+                watchScss(config.outputfolder, config);
+                watchToRefresh(config.outputfolder, config);
             }
 
-            if(argResults.wasParsed(Options._ARG_SERVE)) {
+            if (argResults.wasParsed(Options._ARG_SERVE)) {
                 foundOptionToWorkWith = true;
                 final String port = argResults[Options._ARG_PORT];
-                serve(config.outputfolder,port);
+                serve(config.outputfolder, port);
             }
 
-            if(!foundOptionToWorkWith) {
+            if (!foundOptionToWorkWith) {
                 options.showUsage();
             }
         }
@@ -64,23 +69,39 @@ class Application {
         }
     }
 
-    void serve(final String folder,final String port) {
+    void serve(final String folder, final String port) {
         Validate.notBlank(folder);
         Validate.notBlank(port);
 
-        var staticFiles = new VirtualDirectory(folder)
-            ..allowDirectoryListing = true;
+        final String MY_HTTP_ROOT_PATH = folder; //Platform.script.resolve(folder).toFilePath();
+
+        VirtualDirectory virtDir;
+        void _directoryHandler(final Directory dir,final HttpRequest request) {
+            _logger.info(dir);
+            var indexUri = new Uri.file(dir.path).resolve('index.html');
+            virtDir.serveFile(new File(indexUri.toFilePath()), request);
+        }
+
+        virtDir = new VirtualDirectory(MY_HTTP_ROOT_PATH)
+            ..allowDirectoryListing = true
+            ..followLinks = true
+            ..jailRoot = false
+        ;
+        virtDir.directoryHandler = _directoryHandler;
 
         runZoned(() {
-            HttpServer.bind('0.0.0.0', int.parse(port)).then( (final HttpServer server) {
-                _logger.info('Server running on port: $port');
-                server.listen(staticFiles.serveRequest);
+            HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, int.parse(port)).then( (final HttpServer server) {
+                _logger.info('Server running on port: $port, $MY_HTTP_ROOT_PATH');
+                server.listen( (final HttpRequest request) {
+                    _logger.info("${request.connectionInfo.remoteAddress.address}:${request.connectionInfo.localPort} - ${request.method} ${request.uri}");
+                    virtDir.serveRequest(request);
+                });
             });
         },
         onError: (e, stackTrace) => _logger.severe('Oh noes! $e $stackTrace'));
     }
 
-    void watch(final String folder,final Config config) {
+    void watch(final String folder, final Config config) {
         Validate.notBlank(folder);
         Validate.notNull(config);
 
@@ -94,7 +115,7 @@ class Application {
         });
     }
 
-    void watchScss(final String folder,final Config config) {
+    void watchScss(final String folder, final Config config) {
         Validate.notBlank(folder);
         Validate.notNull(config);
 
@@ -102,39 +123,97 @@ class Application {
         final Directory dir = new Directory(folder);
         final List<File> scssFiles = _listSCSSFilesIn(dir);
 
-        if(scssFiles.length == 0) {
+        if (scssFiles.length == 0) {
             _logger.info("No SCSS files found");
             return;
         }
 
-        scssFiles.forEach((final File file) {
-            file.watch(events: FileSystemEvent.MODIFY).listen( (final FileSystemEvent event) {
-                _logger.info(event.toString());
-
-                final String scssFile = event.path;
-                final String cssFile = "${path.withoutExtension(scssFile)}.css";
-
-                _compileScss(scssFile,cssFile);
-                //_refreshPage(config);
+        File _mainScssFile(final List<File> scssFiles) {
+            final File mainScss = scssFiles.firstWhere((final File file) {
+                final String pureFilename = path.basename(file.path);
+                return pureFilename.startsWith(new RegExp(r"[a-z]", caseSensitive: false));
             });
-        });
+            return mainScss;
+        }
+
+        try {
+            final File mainScss = _mainScssFile(scssFiles);
+
+            final String scssFile = mainScss.path;
+            final String cssFile = "${path.withoutExtension(scssFile)}.css";
+
+            _logger.info("Main SCSS: $scssFile");
+            scssFiles.forEach((final File file) {
+                _logger.info("Observing: ${file.path}");
+
+                file.watch(events: FileSystemEvent.MODIFY).listen((final FileSystemEvent event) {
+                    _logger.fine(event.toString());
+                    //_logger.info("Scss: ${scssFile}, CSS: ${cssFile}");
+
+                    _compileScss(scssFile, cssFile);
+                });
+            });
+
+        }
+        on StateError catch (e) {
+            _logger.info("Found no SCSS without a _ at the beginning...");
+        }
     }
 
-    void watchToRefresh(final String folder,final Config config) {
+    void watchToRefresh(final String folder, final Config config) {
         Validate.notBlank(folder);
         Validate.notNull(config);
 
         _logger.fine('Observing $folder...');
 
-        final List<StreamSubscription<FileSystemEvent>> watchers = new List();
+        void _schedulePageRefresh() {
+            if(timerForPageRefresh == null) {
+                timerForPageRefresh = new Timer(new Duration(milliseconds: 500), () {
+                    _refreshPage(config);
+                    timerForPageRefresh = null;
+                });
+            }
+        }
+
         final File srcDir = new File(folder);
         srcDir.watch(recursive: true).listen((final FileSystemEvent event) {
             _logger.fine(event.toString());
-            _refreshPage(config);
+            _schedulePageRefresh();
         });
     }
 
     // -- private -------------------------------------------------------------
+
+    Future _testPWD(final String folder) async {
+        ProcessResult result = await Process.run("pwd", []);
+        if (result.exitCode != 0) {
+            _logger.info("sassc faild with: ${(result.stderr as String).trim()}!");
+            _vickiSay("got a sassc error");
+            return;
+        }
+
+        final Directory dir = new Directory(folder);
+        dir.watch(recursive: true).listen((final FileSystemEvent event) {
+            _logger.info(event.toString());
+
+            _testPWD2();
+            _compileScss("web/styles/main.scss", "web/styles/main.css");
+
+        });
+
+        _logger.info("PWD1: ${result.stdout.trim()}");
+    }
+
+    Future _testPWD2() async {
+        ProcessResult result = await Process.run("pwd", []);
+        if (result.exitCode != 0) {
+            _logger.info("sassc faild with: ${(result.stderr as String).trim()}!");
+            _vickiSay("got a sassc error");
+            return;
+        }
+
+        _logger.info("PWD11: ${result.stdout.trim()}");
+    }
 
     /**
      * Weitere Infos:
@@ -144,14 +223,16 @@ class Application {
      *
      *      WebSocket / ChromeExtension: http://goo.gl/unsnXc
      */
-    _refreshPage(final Config config) {
+    Future _refreshPage(final Config config) async {
 
-        final ProcessResult result = Process.runSync("pwd", []);
-        if(result.exitCode != 0) {
+        // Nur zum testen!
+        final ProcessResult result = await Process.run("pwd", []);
+        if (result.exitCode != 0) {
             _logger.info("sassc faild with: ${(result.stderr as String).trim()}!");
             _vickiSay("got a sassc error");
             return;
         }
+        _logger.fine(result.stdout.trim());
 
         final String content = """
                 tell application "Chromium"
@@ -173,32 +254,40 @@ class Application {
         final String scriptExtension = "applescript";
 
         final Directory contenfolder = new Directory(config.configfolder);
-        if(!contenfolder.existsSync()) {
-            contenfolder.createSync();
+        if (!await contenfolder.exists()) {
+            await contenfolder.create();
         }
 
         final File script = new File("${contenfolder.path}/$scriptName-${version}.$scriptExtension");
-        if(!script.existsSync()) {
-            contenfolder.listSync().forEach( (final entity) {
+        if (!await script.exists()) {
+            contenfolder.listSync().forEach((final entity) {
                 _logger.info("Entity: ${entity.path}");
-                if(entity is File && path.basename(entity.path).startsWith(scriptName)) {
+                if (entity is File && path.basename(entity.path).startsWith(scriptName)) {
                     final File oldScript = new File(entity.path);
-                    oldScript.deleteSync();
+                    oldScript.delete();
                 }
             });
-            script.writeAsStringSync(content.trim());
+            await script.writeAsString(content.trim());
             _logger.info("${script.path} created...");
         }
 
-        final ProcessResult resultOsascript = Process.runSync(executable, [ script.path ]);
-        if(resultOsascript.exitCode != 0) {
-            _logger.severe("$executable faild with: ${(result.stderr as String).trim()}!");
+        final ProcessResult resultOsascript = await Process.run(executable, [ script.path ]);
+        if (resultOsascript.exitCode != 0) {
+            _logger.severe("$executable faild with: ${(resultOsascript.stderr as String).trim()}!");
             _vickiSay("$executable failed");
             return;
         }
-        _logger.fine("$executable ${script.path} successful!");
+
+        _logger.info("$executable ${script.path} successful!");
     }
 
+    bool _isFolderAvailable(final String folder) {
+        Validate.notBlank(folder);
+        final Directory dir = new Directory(folder);
+        return dir.existsSync();
+    }
+
+    /*
     _compileScss(final String source,final String target) {
         Validate.notBlank(source);
         Validate.notBlank(target);
@@ -212,19 +301,34 @@ class Application {
         }
         _logger.info("Done!");
     }
+*/
+
+    Future _compileScss(final String source, final String target) async {
+        Validate.notBlank(source);
+        Validate.notBlank(target);
+
+        _logger.info("Compiling $source -> $target");
+        final ProcessResult result = await Process.run("sassc", [ source, target ]);
+        if (result.exitCode != 0) {
+            _logger.info("sassc faild with: ${(result.stderr as String).trim()}!");
+            _vickiSay("got a sassc error");
+            return;
+        }
+        _logger.info("Done!");
+    }
 
     List<File> _listSCSSFilesIn(final Directory dir) {
         Validate.notNull(dir);
-        return dir.listSync(recursive: true).where( (final file) {
-            return file is File && file.path.endsWith(".scss");
+        return dir.listSync(recursive: true).where((final file) {
+            return file is File && file.path.endsWith(".scss") && !file.path.contains("packages");
         }).toList();
     }
 
     void _vickiSay(final String sentence) {
         Validate.notBlank(sentence);
 
-        final ProcessResult result = Process.runSync( "say", [ '-v', "Vicki",'-r', '200', sentence.replaceFirst("wsk_","") ]);
-        if(result.exitCode != 0) {
+        final ProcessResult result = Process.runSync("say", [ '-v', "Vicki", '-r', '200', sentence.replaceFirst("wsk_", "") ]);
+        if (result.exitCode != 0) {
             _logger.severe("run faild with: ${(result.stderr as String).trim()}!");
         }
     }
@@ -236,7 +340,7 @@ class Application {
 
         // now control the logging.
         // Turn off all logging first
-        switch(loglevel) {
+        switch (loglevel) {
             case "fine":
             case "debug":
                 Logger.root.level = Level.FINE;
